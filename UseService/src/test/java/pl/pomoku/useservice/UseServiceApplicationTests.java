83b4862b0a038880cfea23ccf77fcc763393fc4a
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-26 08:40:23 +0200

    Initial commit

---
+++ UseService/src/test/java/pl/pomoku/useservice/UseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomoku.useservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
