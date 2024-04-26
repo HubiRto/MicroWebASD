6d30bc86b46ba9af18ad19954e943d9f067e8dc7
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-26 21:46:11 +0200

    Start Algorithm Service

---
+++ AlgorithmService/src/test/java/pl/pomoku/algorithmservice/AlgorithmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomoku.algorithmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
