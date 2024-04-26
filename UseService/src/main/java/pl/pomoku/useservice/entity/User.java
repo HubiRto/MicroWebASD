912474a2b87b8e87c78118f93f577a4d7ca6c857
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-26 09:21:45 +0200

    Start work with UserService

---
+++ UseService/src/main/java/pl/pomoku/useservice/entity/User.java
@@ -0,0 +1,41 @@
+package pl.pomoku.useservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name = "user_seq_gen", sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
---
+++ UseService/src/main/resources/schema.sql
@@ -0,0 +1,31 @@
+CREATE SEQUENCE IF NOT EXISTS user_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS role_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id         BIGINT       NOT NULL DEFAULT nextval('user_seq'),
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   BIGINT              NOT NULL DEFAULT nextval('role_seq'),
+    name VARCHAR(255) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_roles
+(
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_user_email ON users (email);
+CREATE INDEX IF NOT EXISTS idx_role_name ON roles (name);
