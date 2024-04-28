194a90c686b8da97754ec9be3313302ecfbcbf2b
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-28 12:03:45 +0200

    Add test to BinSearch

--- AlgorithmService/src/main/java/pl/pomoku/algorithmservice/controller/AlgorithmController.java
+++ AlgorithmService/src/main/java/pl/pomoku/algorithmservice/controller/AlgorithmController.java
@@ -3,10 +3,7 @@ package pl.pomoku.algorithmservice.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pomoku.algorithmservice.algorithm.BinSearch;
 import pl.pomoku.algorithmservice.dto.input.BinSearchInput;
 import pl.pomoku.algorithmservice.service.BinSearchService;
@@ -16,12 +13,8 @@ import pl.pomoku.algorithmservice.service.BinSearchService;
 @RequiredArgsConstructor
 public class AlgorithmController {
     private final BinSearchService binSearchService;
-    @GetMapping("/hoare-partition")
-    public ResponseEntity<?> hoarePartition(){
-        return null;
-    }
 
-    @GetMapping("/binary-search")
+    @PostMapping("/binary-search")
     public ResponseEntity<?> binSearch(@Valid @RequestBody BinSearchInput input){
         return ResponseEntity.ok(binSearchService.getResult(input));
     }
