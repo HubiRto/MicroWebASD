250e7c78a09a2ec0a1241a8ffad9d88f56930831
hubirto <hubert.rybicki.hr@gmail.com>
2024-05-08 15:17:41 +0200

    Save class DepthFirstSearch for safe

--- AlgorithmService/src/main/java/pl/pomoku/algorithmservice/algorithm/DepthFirstSearch.java
+++ AlgorithmService/src/main/java/pl/pomoku/algorithmservice/algorithm/DepthFirstSearch.java
@@ -7,48 +7,72 @@ import java.util.*;
 public class DepthFirstSearch {
     private static int pushOperationCount = 0;
     private static int popOperationCount = 0;
-    private static int internalVertexesCount = 0;
+    private static int leafCount = 0;
+    private static int internalCount = 0;
+    private static int maxDepth = 0;
+    private static int maxStackSize = 0;
+    private static final Map<Vertex<Integer>, Vertex<Integer>> parentMap = new HashMap<>();
+
     public static void traverse(Vertex<Integer> startVertex) {
         Deque<Vertex<Integer>> stack = new LinkedList<>();
-        Set<Integer> externalVertices = new HashSet<>();
 
         startVertex.setVisited(true);
+        startVertex.setDepth(0);
         stack.push(startVertex);
+        maxStackSize++;
         pushOperationCount++;
+        parentMap.put(startVertex, null);
 
-        boolean contains = false;
         while (!stack.isEmpty()) {
             Vertex<Integer> current = stack.pop();
             popOperationCount++;
             System.out.println(current);
+
+            maxDepth = Math.max(maxDepth, current.getDepth());
+            boolean hasUnvisitedNeighbors = false;
+
             for (Vertex<Integer> ngh : current.getNeighbors()) {
                 if (!ngh.isVisited()) {
                     stack.push(ngh);
+                    if(maxStackSize < stack.size()) {
+                        maxStackSize = stack.size();
+                    }
                     ngh.setVisited(true);
-
+                    ngh.setDepth(current.getDepth() + 1);
+                    hasUnvisitedNeighbors = true;
+                    parentMap.put(ngh, current);
                     pushOperationCount++;
-                    contains = true;
-                    externalVertices.add(ngh.getData());
                 }
             }
 
-            if(contains){
-                internalVertexesCount++;
-                externalVertices.remove(current.getData());
+            if (hasUnvisitedNeighbors) {
+                internalCount++;
+            } else {
+                leafCount++;
             }
         }
 
         System.out.println("-------------------------------");
         System.out.println("Liczba operacji PUSH: " + pushOperationCount);
         System.out.println("Liczba operacji POP: " + popOperationCount);
+        System.out.println("Liczba wierzchołków zewnętrznych: " + leafCount);
+        System.out.println("Liczba wierzchołków wewnętrznych: " + internalCount);
+        System.out.println("Maksymalna głębokość drzewa: " + maxDepth);
+        System.out.println("Maksymalna wysokosc stosu: " + maxStackSize);
         System.out.println("-------------------------------");
-        System.out.println("Liczba wierzchołków WEWNĘTRZNYCH: " + internalVertexesCount);
-        System.out.println("Liczba wierzchołków ZEWNĘTRZNYCH: " + externalVertices.size());
-        System.out.println("-------------------------------");
-//        System.out.println("Maksymalana wysokość stosu: " + stack.getCounter().getMaxSize());
-////        System.out.println("Końcowa wysokość stosu: " + stack.size());
-//        System.out.println("Wysokość drzewa rozpinającego: " + maxHeight);
-//        System.out.println("-------------------------------");
+    }
+
+    public static void printPaths(List<Vertex<Integer>> vertices, Vertex<Integer> startVertex) {
+        for (Vertex<Integer> vertex : vertices) {
+            if (vertex == startVertex) {
+                continue;
+            }
+            LinkedList<Integer> path = new LinkedList<>();
+            for (Vertex<Integer> at = vertex; at != null; at = parentMap.get(at)) {
+                path.addFirst(at.getData());
+            }
+            System.out.println("Ścieżka od " + startVertex.getData() + " do " + (vertex != null ? vertex.getData() : null) + ": " + path);
+        }
     }
 
     public static void calculate(int[][] graph, int startVertexIdx) {
@@ -63,7 +87,9 @@ public class DepthFirstSearch {
             }
         }
 
-        traverse(vertices.get(startVertexIdx));
+        Vertex<Integer> startVertex = vertices.get(startVertexIdx);
+        traverse(startVertex);
+        printPaths(vertices, startVertex);
     }
 
     public static void main(String[] args) {
@@ -77,17 +103,50 @@ public class DepthFirstSearch {
 //                {1, 2, 3, 4, 5},
 //                {0, 2, 3, 4, 5}
 //        };
+//        int[][] graph = {
+//                {4, 5},
+//                {3, 5, 6, 7},
+//                {3},
+//                {1, 2, 7},
+//                {0, 5},
+//                {0, 1, 4, 6, 7},
+//                {1, 5},
+//                {1, 3, 5}
+//        };
+
         int[][] graph = {
-                {4, 5},
-                {3, 5, 6, 7},
-                {3},
-                {1, 2, 7},
-                {0, 5},
-                {0, 1, 4, 6, 7},
-                {1, 5},
-                {1, 3, 5}
+                {2,6},
+                {2,4,5,6,7},
+                {0,1,4,5,7},
+                {5},
+                {1,2},
+                {1,2,3},
+                {0,1},
+                {1,2}
         };
 
-        calculate(graph, 0);
+//        int[][] graph = {
+//                {2,4},
+//                {4,5,6},
+//                {0,4,5},
+//                {6,7},
+//                {0,1,2},
+//                {1,2},
+//                {1,3,7},
+//                {3,6}
+//        };
+
+//        int[][] graph = {
+//                {1,2,4,7},
+//                {0,2,4,5},
+//                {0,1,3,7},
+//                {2,4,5,6},
+//                {0,1,3,7},
+//                {1,3,6},
+//                {3,5},
+//                {0,2,4}
+//        };
+
+        calculate(graph, 3);
     }
 }
